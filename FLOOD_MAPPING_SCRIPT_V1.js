d8dcd82c53494e5ab093d4986c6b6a121bd248b6
Chrysanthus-Jumaa <160774354+Chrysanthus-Jumaa@users.noreply.github.com>
2025-07-16 07:07:36 +0300

    Update FLOOD_MAPPING_SCRIPT_V1.js
    I have updated the code with an improved version of the same. It is well commented. As for the ROI, I shall share it with you right away. Find the link to the code as at this time below:
    https://code.earthengine.google.com/e7bbc3a6ef8fa29607f4744bd05542c1
    This code only marks the first step, which is simply obtaining the SAR imagery, filtering and processing the imagery, accordingly, obtaining threshold values and masking to obtain flooded regions. I shall update you guys on the next phase which is integrating NDWI from Sentinel-2 MSI and share the corresponding link to the same accordingly. Rest assured

--- FLOOD_MAPPING_SCRIPT_V1.js
+++ FLOOD_MAPPING_SCRIPT_V1.js
@@ -1,118 +1,101 @@
-var Slope = ee.Image("WWF/HydroSHEDS/03VFDEM");
+// IMPORTING MY SENTINEL-1 SAR IMAGERY
 var s1 = ee.ImageCollection("COPERNICUS/S1_GRD");
-Map.addLayer(geometry);
-Map.centerObject(geometry, 9);
 
-//lets select the images  by predefined dates
+// ADDING MY ROI TO THE VIEWPORT AND POSITIONING IT ACCORDINGLY FOR EASIER VIEW
+Map.addLayer(roi);
+Map.centerObject(roi, 16);
+
+// DEFINING MY PRE-FLOOD DATES AND POST-FLOOD DATES
 var before_start = '2017-07-15';
 var before_end = '2019-08-10';
 var after_start = '2019-08-10';
 var after_end = '2023-03-23';
 
-// lets first of all import our Imagery-- Put key note on the method of transmittance & reflectance,
-//the resolution and etc
-//some of these can be found under the imagery properties
-var filtered =  s1
-.filter(ee.Filter.eq('instrumentMode', 'IW')) 
-.filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VH'))
-.filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV'))
-.filter(ee.Filter.eq('orbitProperties_pass','ASCENDING'))
-.filter(ee.Filter.eq('resolution_meters',10))
-.select(['VH'])
+// FILTERING MY IMPORTED SENTINEL-1 SAR IMAGERY
+var filteredS1 = s1
+  .filter(ee.Filter.eq('instrumentMode', 'IW')) //FETCHING IN INTERFERMETRIC WIDE MODE FOR MORE IMAGE COVERAGE
+  .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VH'))//UTILISING THE VH BAND
+  .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV'))//UTILISING THE VV BAND
+  .filter(ee.Filter.eq('orbitProperties_pass','ASCENDING'))//OBTAINUNG IMAGES FROM THE ASCENDING PATH
+  .filter(ee.Filter.eq('resolution_meters', 10))//FILTERING TO GET 10M RESOLUTION IMAGERY
+  .select(['VH']);
 
-// lets now filter our imagery within our preferred date
-var beforeCollection = filtered.filter(ee.Filter.date(before_start,before_end))
-var afterCollection = filtered.filter(ee.Filter.date(after_start,after_end))
+// OBTAINING IMAGERY BEFORE AND AFTER FLOODING
+var beforeCollection = filteredS1.filter(ee.Filter.date(before_start,before_end));
+var afterCollection = filteredS1.filter(ee.Filter.date(after_start,after_end));
 
-//lets clip within our AOI & Apply the Mosaic function
+// CREATING MY MOSAICS FOR PREFLOOD IMAGERY AND POSTFLOOD IMAGERY
+var beforemosaic = beforeCollection.mosaic();
+var aftermosaic = afterCollection.mosaic();
 
-var before =  beforeCollection.mosaic().clip(geometry)
-var after =  afterCollection.mosaic().clip(geometry)
-//lets print our first imagery
-print(filtered.first())
-print(before)
-print(after)
+// CLIPPING THE TWO MOSAICS
+var beforeclip = beforemosaic.clip(roi);
+var afterclip = aftermosaic.clip(roi);
 
-// //LETS COME UP WITH AN RGB imagery for before & after 
-// var addRatioBand = function(image){
-//   var ratioBand = image.select('VH').divide(image.select('VH')).rename('VV/VH')
-//   return image.addBands(ratioBand)
-// }
-// var beforeRGB = addRatioBand(before)
-// var afterRGB = addRatioBand(after)
-// print(beforeRGB)
-// //lets add the visual parameters
-// var visParams = {
-//   min: [-25,-25,0],
-//   max: [0,0,2]
-// }
-// //lets visualize the imagery
-// Map.addLayer(beforeRGB,visParams,'BeforeRGB')
-// Map.addLayer(afterRGB,visParams,'AfterRGB')
+// ADDING THE CLIPPED MOSAICS TO THE VIEWPORT
+Map.addLayer(beforeclip,{min:-25,max:0},'Before Floods',false);
+Map.addLayer(afterclip,{min:-25,max:0},'After Floods',false);
 
-
-Map.addLayer(before,{min:-25,max:0},'Before Floods',false)
-Map.addLayer(after,{min:-25,max:0},'After Floods',false)
-
-
- // Function to convert to dB
+// CREATING A FUNCTION TO CONVERT IMAGE VALUES TO NATURAL UNITS
 function toNatural(img){
   return ee.Image(10.0).pow(img.select(0).divide(10.0));
 }
 
-// Function to convert to dB
+//  CREATING A FUNCTION TO CONVERT IMAGE VALUES TO DECIBELS
 function toDb(img) {
   return ee.Image(img).log10().multiply(10.0);
 }
 
-// Let's now apply the speckle
+// CORRECTING THE IMAGE FOR SPECKLE
 function RefinedLee(img){
-  // Image must be in the natural unit i.e., not in dB!
-  // Set up 3x3 kernels
+  // WE APPLY SPECKLE TO IMAGERY IN NATURAL UNITS AND NOT DECIBELS
+  // SETTING UP A 3X3 KERNEL
   var weights3 = ee.List.repeat(ee.List.repeat(1,3),3);
   var kernel3 = ee.Kernel.fixed(3, 3, weights3, 1, 1, false);
 }
 
-var beforeFiltered = ee.Image(toDb(RefinedLee(toNatural(before))));
-var afterFiltered = ee.Image(toDb(RefinedLee(toNatural(after))));
+//WE CONVERT THE IMAGERY TO NATURAL UNITS, THEN APPLY THE SPECKLE FILTER BEFORE CONVERTING
+//EVERYTHING BACK TO DECIBELS
+var beforespecklecorrected = ee.Image(toDb(RefinedLee(toNatural(beforeclip))));
+var afterspecklecorrected = ee.Image(toDb(RefinedLee(toNatural(afterclip))));
 
-Map.addLayer(beforeFiltered, {min: -25, max: 0}, 'Before Filtered', false);
-Map.addLayer(afterFiltered, {min: -25, max: 0}, 'After Filtered', false);
+//ADDING THE SPECKLE CORRECTED IMAGERY TO THE VIEWPORT
+Map.addLayer(beforespecklecorrected, {min: -25, max: 0}, 'Before specklecorrected', false);
+Map.addLayer(afterspecklecorrected, {min: -25, max: 0}, 'After specklecorrected', false);
 
-var difference = after.divide(before); 
-var diffThreshold = 1.5;
+// OBTAINING MY RATIO OF AFTER FLOOD IMAGERY TO BEFORE FLOOD IMAGERY
+var myratio = afterclip.divide(beforeclip); 
 
-var flooded = difference.gt(diffThreshold).rename(['Water']).selfMask(); 
-Map.addLayer(flooded, {min: 0, max: 1, palette: ['orange']}, 'Initial Flood Initiate');
-
-var permanentwater = ee.Image("JRC/GSW1_4/GlobalSurfaceWater").select('seasonality').gt(5).clip(geometry)
-var flooded = flooded.updateMask(permanentwater)
-
-var slopeThreshold = 5;
-var terrain = ee.Algorithms.Terrain(Slope);
-var slope = terrain.select('slope'); // Select the 'slope' band
-
-var flooded = flooded.updateMask(slope.lt(slopeThreshold));
-
-var connectedPixelThreshold = 2;
-var connections = flooded.connectedPixelCount(25);
-
-var flooded = flooded.updateMask(connections.gt(connectedPixelThreshold));
-
-Map.addLayer(flooded, {min: 0, max: 1, palette: ['Red']}, 'Flooded Area', false);
-
-// var totalarea = geometry.area();
-// print(totalarea);
-
-//lets try to print the area of the flooded areas
-
-var stats = flooded.multiply(ee.Image.pixelArea()).reduceRegion({
-  reducer:ee.Reducer.sum(),
-  geometry:geometry,
-  scale:30,
-  maxPixels:1e10,
-  tileScale:16})
+// FUNCTION TO APPLY A PERCENTILE BASED FLOOD MASK INSTEAD OF ASSIGNING ARBITRATY THRESHOLDS
+function applyPercentileMask(img, region, percentile) {
+  var bandName = img.bandNames().get(0);
   
-print(stats)
-var floodedArea = ee.Number(stats.get('Water')).divide(10000)
-print (floodedArea)
+  var threshold = img.reduceRegion({
+    reducer: ee.Reducer.percentile([percentile]),
+    geometry: region,
+    scale: 10,
+    maxPixels: 1e13
+  }).get(bandName);
+  
+  var mask = img.gt(ee.Image.constant(threshold)).rename(['Flooded']).selfMask();
+  return mask;
+}
+
+// MAPPING MY RATIOED IMAGE TO THE FUNCTION 
+var dynamicFlood = applyPercentileMask(myratio, roi, 90);
+Map.addLayer(dynamicFlood, {min: 0, max: 1, palette: ['red']}, 'Flood Mask - Percentile (90)');
+
+//THIS SECTION WAS FOR EXPORTING MY ROI AS SHAPEFILE TO HAVE PERMANENT ROI IN PLACE////////
+// Convert your drawn geometry to a FeatureCollection
+// var aoiFeature = ee.FeatureCollection([ee.Feature(roi)]);
+
+// // Export to Google Drive as a shapefile
+// Export.table.toDrive({
+//   collection: aoiFeature,
+//   description: 'AOI_Shapefile_Export',
+//   folder:'FLOOD_MAPPING_PROJECT',
+//   fileNamePrefix:'THE_ROI',
+//   fileFormat: 'SHP'
+// });
+////////////////////////////UTILISING NDWI FOR FLOOD MAPPING/////////////////////////////
+
